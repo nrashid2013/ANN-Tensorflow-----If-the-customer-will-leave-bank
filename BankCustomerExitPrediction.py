08541c43d4d9d0f9c7673b05600bcec6b1d4993f
Nehal Rashid <nehal.rashid@gmail.com>
2019-09-28 17:19:04 -0500

    Update BankCustomerExitPrediction.py

--- BankCustomerExitPrediction.py
+++ BankCustomerExitPrediction.py
@@ -73,4 +73,6 @@ y_pred = (y_pred > 0.5)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix,accuracy_score
 cm = confusion_matrix(y_test, y_pred)
+print(cm)
 accuracy=accuracy_score(y_test,y_pred)
+print(accuracy)
